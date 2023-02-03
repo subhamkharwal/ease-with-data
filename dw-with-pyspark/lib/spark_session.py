1feb6f448e9b4fc1368c4c2ea65a424bb6b3730d
Subham Khandelwal <subhamkharwal@gmail.com>
2023-02-03 06:34:35 +0000

    Added common code

---
+++ dw-with-pyspark/lib/spark_session.py
@@ -0,0 +1,17 @@
+from pyspark.sql import SparkSession
+from pyspark import SparkConf
+
+def get_spark_session(_appName: "Default AppName") -> SparkSession:
+    
+    # Create blank conf to get the configurations
+    conf = SparkConf() \
+        .setAppName(_appName)
+    
+    # Create the SparkSession
+    spark = SparkSession \
+        .builder \
+        .config(conf=conf) \
+        .enableHiveSupport() \
+        .getOrCreate()
+    
+    return spark
