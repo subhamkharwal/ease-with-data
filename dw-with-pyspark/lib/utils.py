9775ca42035eb6cc178dfeb0aa6ed66077c4ac20
Subham Khandelwal <subhamkharwal@gmail.com>
2023-02-07 05:31:09 +0000

    Fix for rundate

--- dw-with-pyspark/lib/utils.py
+++ dw-with-pyspark/lib/utils.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 from pyspark.sql import SparkSession, DataFrame, udf
 from pyspark.sql.types import StringType
+import json
 
 # Date Utility to generate Date source data for next 1 year
 def date_data(start_run_dt: str = '20230101', num_years: int = 1) -> list:
@@ -30,4 +31,15 @@ def get_string_cols(spark: SparkSession, df: DataFrame) -> list:
     for col in df.columns:
         _col_list.append(f"cast({col} as string) as {col}")
     return _col_list
+
+# Get rundate from config
+def get_rundate() -> str:
+    try:
+        with open("run_config.txt", "r") as f:
+            data = json.load(f)
+            f.close()
+        return data['rundate']
+    except Exception as e:
+        print(e)
+        return "19000101"
 
