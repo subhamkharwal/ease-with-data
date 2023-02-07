50d6bee78fef67b8ca649ff9c9e6268789ba9782
Subham Khandelwal <subhamkharwal@gmail.com>
2023-02-07 11:16:26 +0000

    Added Sales Fact landing

--- dw-with-pyspark/lib/aws_s3.py
+++ dw-with-pyspark/lib/aws_s3.py
@@ -23,12 +23,29 @@ def archive_landing_object(filename: str, dataset: str) -> bool:
     try:
         client = create_s3_client()
         copy_source = {'Bucket': bucket_name, 'Key': f"dw-with-pyspark/landing/{dataset}/{filename}"}
-        response = client.copy_object(Bucket = 'easewithdata', CopySource = copy_source, Key = f"dw-with-pyspark/archive/{dataset}/{filename}")
+        response = client.copy_object(Bucket = bucket_name, CopySource = copy_source, Key = f"dw-with-pyspark/archive/{dataset}/{filename}")
         if response['ResponseMetadata']['HTTPStatusCode'] == 200:
             client.delete_object(Bucket = bucket_name, Key = f"dw-with-pyspark/landing/{dataset}/{filename}")
             return True
         else:
             return False
+    except Exception as e:
+        print(e)
+        return False
+    
+def archive_wildcard_landing_objects(filename: str, dataset: str) -> bool:
+    try:
+        _file = filename.split("*")[0]
+        _bucket = create_s3_session().Bucket(bucket_name)
+        _file_list = [i.key for i in _bucket.objects.all() if 'landing' in i.key and _file in i.key]
+        #print(_file_list)
+        for key in _file_list: 
+            client = create_s3_client()
+            copy_source = {'Bucket': bucket_name, 'Key': f"{key}"}
+            response = client.copy_object(Bucket = bucket_name, CopySource = copy_source, Key = f"{str(key).replace('landing', 'archive')}")
+            if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+                client.delete_object(Bucket = bucket_name, Key = f"{key}")
+        return True
     except Exception as e:
         print(e)
         return False
